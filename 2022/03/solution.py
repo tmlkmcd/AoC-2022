b6d05b6e9e82593eec2b338933cb90f002647f74
Tommy <thomas.mcdevitt@tractable.ai>
2022-12-03 18:17:07 +0000

    Day 3 complete.

---
+++ 2022/03/solution.py
@@ -0,0 +1,40 @@
+import sys
+
+lines_in = [line.strip() for line in sys.stdin]
+
+
+def to_priority(letter):
+    ascii_code = ord(letter)
+    return ascii_code - 38 if ascii_code <= 90 else ascii_code - 96
+
+
+assert(to_priority('a') == 1)
+assert(to_priority('z') == 26)
+assert(to_priority('A') == 27)
+assert(to_priority('Z') == 52)
+
+dupes = []
+
+for backpack in lines_in:
+    assert(len(backpack) % 2 == 0)
+    half = int(len(backpack) / 2)
+    for c in backpack[half:]:
+        if c in backpack[:half]:
+            dupes.append(c)
+            break
+
+pointer = 0
+badges = []
+
+while pointer < len(lines_in):
+    group = lines_in[pointer:pointer + 3]
+    assert(len(group) == 3)
+    pointer += 3
+
+    for c in group[0]:
+        if c in group[1] and c in group[2]:
+            badges.append(c)
+            break
+
+print('part 1', sum([to_priority(i) for i in dupes]))
+print('part 2', sum([to_priority(b) for b in badges]))
--- 2022/README.md
+++ 2022/README.md
@@ -9,3 +9,9 @@ Easy peasy. Got snagged for a minute on parsing the empty line between each elf 
 - Estimated completion time: ~20 minutes
 
 Hard-coding ftw. Fumbled a lot on trying to figure out a more dynamic strategy before deciding to simply hard-code everything. I'm TOO TIRED!!
+
+## Day 3
+
+- Estimated completion time: ~15 minutes
+
+Spent a long time fumbling over part 1 because I wasn't sure if I had understood the question correctly (unique duplicates or just all duplicates?) - my solution kept coming up with a number that was way too high then randomly output the right answer without any change on my part that I knew of. Part 2 went down on the first try (about 2 minutes) which is atypical for me.
